9152b50cb5f0ca4f3d49dc8a64d54b1489d9d95d
laky <lukes.o@post.cz>
2025-01-04 16:45:41 +0100

    responsivity, animations, references header

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ function navbarDropDown() {
     let control = document.getElementById("nav_drop")
     if (control.style.display == "flex") {
         control.style.display = "none"
-    } else control.style.display = "flex"
+        document.getElementById("hamburger_bars").style.display = "flex"
+        document.getElementById("hamburger_x").style.display = "none"
+    } else {
+        control.style.display = "flex"
+        document.getElementById("hamburger_bars").style.display = "none"
+        document.getElementById("hamburger_x").style.display = "flex"
+    } 
 }
